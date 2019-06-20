bde8389d4d8c3ee54baaf7e37de4fe3a96436f24
Brent Labasan <BrentLabasan@gmail.com>
2019-06-20 16:11:47 -0700

    Update compose.js

--- src/compose.js
+++ src/compose.js
@@ -16,8 +16,8 @@ export default function compose(...funcs) {
     }
 
     const last = funcs[funcs.length - 1]
-    const rest = funcs.slice(0, -1) //BVL A negative index can be used, indicating an offset from the end of the sequence. slice(0, -1) extracts the first element through the second-to-last element in the sequence.
-
-    return rest.reduceRight((composed, f) => f(composed), last(...args)) //BVL https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/ReduceRight
+    const rest = funcs.slice(0, -1)
+    
+    return rest.reduceRight((composed, f) => f(composed), last(...args))
   }
 }
